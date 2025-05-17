973ed2c80480041ef956b7add673ed571dd81440
TheBlackEagle <150778165+SkDAnS@users.noreply.github.com>
2025-05-17 19:48:28 +0200

    Update ghost_websocket_server.erl

--- ghost_backend/ghost_websocket_server.erl
+++ ghost_backend/ghost_websocket_server.erl
@@ -1,9 +1,3 @@
-
-%%%==============================================================
-%%% Fichier : ghost_websocket_server.erl
-%%% Description : Démarre le serveur Cowboy avec WebSocket
-%%%==============================================================
-
 -module(ghost_websocket_server).
 -export([start/0, stop/0]).
 
@@ -11,17 +5,18 @@ start() ->
     ghostinthethread:init(),
     application:ensure_all_started(cowboy),
 
-    Dispatch = cowboy_router:compile([
+    Dispatch = [
         {'_', [
             {"/websocket", ghost_websocket_handler, []}
         ]}
-    ]),
+    ],
 
     {ok, _} = cowboy:start_clear(ghost_http_listener,
         [{port, 8080}],
-        #{env => #{dispatch => Dispatch}}
+        #{env => #{dispatch => cowboy_router:compile(Dispatch)}}
     ),
-    io:format("WebSocket server started on port 8080~n").
+    io:format("WebSocket server started on port 8080~n"),
+    ok.
 
 stop() ->
     cowboy:stop_listener(ghost_http_listener).
